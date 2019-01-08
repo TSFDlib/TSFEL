1482c4a879a0c8a08998a58397551f09b685fc23
TSFDlib <45789895+TSFDlib@users.noreply.github.com>
2019-01-08 10:59:21 +0000

    Update __init__.py

--- tsfel/__init__.py
+++ tsfel/__init__.py
@@ -1,6 +1,5 @@
-#from TSFEL.tsfel.feature_extraction import *
+
 #from TSFEL.tsfel.features_report import *
-#from TSFEL.tsfel.utils import *
+from TSFEL.tsfel.utils import *
 from TSFEL.tsfel.feature_extraction import *
-#from TSFEL.features_report import *
-from utils import *
+
