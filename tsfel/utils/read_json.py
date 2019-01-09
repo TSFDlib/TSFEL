02ec35c2fe0e2bc7bf0e9f1ba32ca80cf0582686
TSFDlib <45789895+TSFDlib@users.noreply.github.com>
2019-01-09 11:46:18 +0000

    Update read_json.py

--- tsfel/utils/read_json.py
+++ tsfel/utils/read_json.py
@@ -170,3 +170,88 @@ def feat_extract(dictionary, signal_window, signal_label, FS = 100, iteration=No
         res+=[s[1]]
 
     return res, nam
+
+
+def one_extract(feat_dict, signal_window, FS = 100, iteration=None):
+    """
+    This function computes features matrix for one window.
+    :param dictionary: (json file)
+           list of features
+    :param signal_window: (narray-like)
+           input from which features are computed, window.
+    :param signal_label: (narray-like)
+           one of axes of acelerometer.
+    # :param : (int)
+    #        sampling frequency
+    :return: res: (narray-like)
+             values of each features for signal.
+             nam: (narray-like)
+             names of the features
+    """
+
+    #Create global arrays
+    func_total = []
+    func_names=[]
+    imports_total=[]
+    parameters_total=[]
+    free_total=[]
+
+
+    #Read Function (real name of function)
+    function = feat_dict['function']
+    #Read Imports
+    imports = str(feat_dict['imports'])
+
+    #Read Parameters
+    parameters = feat_dict['parameters']
+
+    #Read Free Parameters
+    free_parameters = feat_dict['free parameters']
+    #Execute imports
+    exec(imports)
+
+    nfuncs = len(func_total)
+    func_results = []
+    name=[]
+    names=[]
+
+    if iteration==None:
+
+        execf = function + '(signal_window'
+
+        if parameters != '':
+            execf += ', '+ parameters
+
+        if free_parameters != '' :
+            for n, v in free_parameters.items():
+                #TODO: conversion may loose precision (str)
+                execf += ', ' + n + '=' + str(v[0])
+
+        execf += ')'
+
+    eval_result = eval( execf, locals())
+
+    #Function returns more than one element
+    if type(eval_result) == tuple:
+        for rr in range(len(eval_result)):
+            if np.isnan(eval_result[0]):
+                eval_result=np.zeros(len(eval_result))
+            func_results += [eval_result[rr]]
+
+        #Low g sum, for total acceleration
+        if func_total[i] == 'hist_json' and signal_label=='tot':
+
+            nbins = dictionary['statistical']['histogram']['free parameters']['nbins'][0]
+            factor= int(0.10*nbins)
+            low = func_results[:factor]
+            low_g = sum(low)
+            func_results+=[low_g]
+
+    else:
+
+        func_results += [eval_result]
+
+
+    #To sort names and func_results (to the same order)
+
+    return func_results
