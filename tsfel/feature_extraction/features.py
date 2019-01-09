7fac61121a7e5e78f837c2a72df271aedce70f90
TSFDlib <45789895+TSFDlib@users.noreply.github.com>
2019-01-09 14:36:15 +0000

    Update features.py

--- tsfel/feature_extraction/features.py
+++ tsfel/feature_extraction/features.py
@@ -3,11 +3,38 @@
 from scipy.stats.stats import pearsonr
 from scipy.stats import kurtosis, skew
 from novainstrumentation.peaks import bigPeaks
-from novainstrumentation.tools import plotfft
+#from novainstrumentation.tools import plotfft
 import numpy as np
 ########################################################################################################################
 # ####################################  TEMPORAL DOMAIN  ############################################################# #
 ########################################################################################################################
+
+
+def plotfft(s, fmax, doplot=False):
+    """ This functions computes the fft of a signal, returning the frequency
+    and their magnitude values.
+    Parameters
+    ----------
+    s: array-like
+      the input signal.
+    fmax: int
+      the sampling frequency.
+    doplot: boolean
+      a variable to indicate whether the plot is done or not.
+    Returns
+    -------
+    f: array-like
+      the frequency values (xx axis)
+    fs: array-like
+      the amplitude of the frequency values (yy axis)
+    """
+
+    fs = abs(np.fft.fft(s))
+    f = linspace(0, fmax / 2, len(s) / 2)
+    if doplot:
+        pl.plot(f[1:len(s) // 2], fs[1:len(s) // 2])
+    return (f[1:len(s) // 2].copy(), fs[1:len(s) // 2].copy())
+
 
 
 # Autocorrelation
