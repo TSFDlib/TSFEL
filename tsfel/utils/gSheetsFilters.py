51de782c49f09618f0350ac8b6be3e169b45da9b
TSFDlib <45789895+TSFDlib@users.noreply.github.com>
2019-01-10 15:19:01 +0000

    Update gSheetsFilters.py

--- tsfel/utils/gSheetsFilters.py
+++ tsfel/utils/gSheetsFilters.py
@@ -59,7 +59,7 @@ def extract_sheet():
             for feat in DICTIONARY[domain].keys():
                 if feat not in list_of_features:
                     feat_dict = DICTIONARY[domain][feat]
-                    if feat_dict['free parameters'] == '':
+                    param = feat_dict['free parameters']
                         param = ''
                     if feat_dict['parameters'] == 'FS':
                         param = str({"fs":100})
