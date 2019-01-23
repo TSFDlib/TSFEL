c500758750d1f21a97c8e0b2a44d4d74c57b2ecf
TSFDlib <45789895+TSFDlib@users.noreply.github.com>
2019-01-23 11:07:20 +0000

    Add files via upload

--- tsfel/utils/gSheetsFilters.py
+++ tsfel/utils/gSheetsFilters.py
@@ -61,7 +61,7 @@ def extract_sheet(gSheetName):
                     feat_dict = DICTIONARY[domain][feat]
                     param = ''
                     if feat_dict['free parameters']:
-                        param = str({"nbins": [10], 'r': [1]})
+                        param = str({"nbins": 10, 'r': 1})
                     if feat_dict['parameters'] == 'FS':
                         param = str({"fs": 100})
                     curve = feat_dict['Complexity']
@@ -86,7 +86,7 @@ def extract_sheet(gSheetName):
         if use_or_not[i] == 'TRUE':
             if list_of_features[i] == 'Histogram':
                 val = sheet.cell(i + 5, 5).value
-                DICTIONARY['Statistical'][list_of_features[i]]['free parameters'] = str({'nbins': [ast.literal_eval(val)['nbins']], "r": [ast.literal_eval(val)['r']]})
+                DICTIONARY['Statistical'][list_of_features[i]]['free parameters'] = {'nbins': ast.literal_eval(val)['nbins'], "r": ast.literal_eval(val)['r']}
             DICTIONARY['Statistical'][list_of_features[i]]['use'] = 'yes'
 
     for i in range(len_temp):
